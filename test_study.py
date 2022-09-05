04d4e33d3a57790860a5c24b19c00bc1bb318cdb
stnslv27 <tilkzv1@mail.ru>
2022-09-06 00:02:38 +0300

    Сделал негативный тест на поиск

---
+++ test_study.py
@@ -0,0 +1,6 @@
+from selene.support.shared import browser
+from selene import be, have
+def test_study():
+    browser.open('https://google.com/ncr')
+    browser.element('[name="q"]').should(be.blank).type('d66d6d6dsss').press_enter()
+    browser.element('[id="topstuff"]').should(have.text('d66d6d6dsss'))
