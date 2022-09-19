57756da149f6ae501045dd8673d5c0a2d645123f
stnslv27 <tilkzv1@mail.ru>
2022-09-19 20:00:20 +0300

    Обновил негативный тест на поиск

--- test_study.py
+++ test_study.py
@@ -1,6 +1,7 @@
 from selene.support.shared import browser
 from selene import be, have
 def test_study():
-    browser.open('https://google.com/ncr')
+    browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('d66d6d6dsss').press_enter()
     browser.element('[id="topstuff"]').should(have.text('d66d6d6dsss'))
+
